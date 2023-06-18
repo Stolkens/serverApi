c723ee0b9d65fe466319edab8cba69e8dbd7061c
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-06-19 00:14:38 +0200

    Fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const db = require('./db');
+const path = require('path');
 
 const app = express();
 
@@ -16,11 +17,17 @@ app.use(cors());
 app.use('/api', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
